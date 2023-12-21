348c0c74dc41b2c5e2f39c4b87fbc119c29e4c71
sksunil0608 <sksunil0608@gmail.com>
2023-12-21 20:50:45 +0530

    chnaged base url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 
 const apiConfig = {
-    baseURL: process.env.REACT_APP_BASE_URL || 'http://127.0.0.1:4000',
+    baseURL: process.env.REACT_APP_BASE_URL || 'http://44.219.93.131',
 };
 
 export { apiConfig };
