5058f53f06d6184d67354755c82cba5029692bc0
sksunil0608 <sksunil0608@gmail.com>
2024-01-06 03:11:42 +0530

    backend url again change

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 
 const apiConfig = {
-    baseURL: process.env.REACT_APP_BASE_URL || 'https://chatapp-backend.vismrit.com',
+    baseURL:'https://chatapp-backend.vismrit.com',
 };
 
 export { apiConfig };
